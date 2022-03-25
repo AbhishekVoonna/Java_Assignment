b2cfe672bd48770a51f82e3292fb143cd04fc489
AbhishekVoonna <102207046+AbhishekVoonna@users.noreply.github.com>
2022-03-25 12:56:56 +0530

    Create sort_arr.java

---
+++ sort_arr.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class sort_arr {
+ public static void main(String args[]) {
+  int[] arr = {23,92,56,39,93,78,64};
+  System.out.println("Before Sort : "+Arrays.toString(arr));
+  Arrays.sort(arr);
+  System.out.println("After Sort : "+Arrays.toString(arr));
+ }
+}
